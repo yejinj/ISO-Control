3cd521876d244838d506e7cd8f9c40146e7d8167
yejinj <sitting077@gmail.com>
2025-06-02 13:17:42 +0900

    fix: 구조 오류로 인한 포트 불일치 문제 해결

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -19,8 +19,8 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: List[str] = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
     ]
     
     # 쿠버네티스 설정
